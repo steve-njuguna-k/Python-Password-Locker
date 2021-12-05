8f2319ca1d5fa8fa17a7d2248e315cdee6464756
Steve Njuguna <stevenjuguna93@gmail.com>
2021-12-05 15:42:15 +0300

    Added a save user function

--- user.py
+++ user.py
@@ -4,3 +4,6 @@ class User:
     def __init__(self, username, password):
         self.username = username
         self.password = password
+
+    def save_user(self):
+        User.user_list.append(self)
