96b609b46c1d75ce787c356d88e0b8ded3f90204
Steve Njuguna <stevenjuguna93@gmail.com>
2021-12-05 16:50:40 +0300

    Added Credentials class & init function

---
+++ credentials.py
@@ -0,0 +1,5 @@
+class Credentials:
+    def __init__(self, accountName, accountUsername, accountPassword):
+        self.accountName = accountName
+        self.accoutnUsername = accountUsername
+        self.accountPassword = accountPassword
