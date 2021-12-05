08dbb1908f3db04ab4438abd7384373104fd3d35
Steve Njuguna <stevenjuguna93@gmail.com>
2021-12-05 16:00:54 +0300

    Added create new user function & imported User class into it

--- run.py
+++ run.py
@@ -4,6 +4,10 @@ def login(username, password):
     check_user =  User.verify_user(username, password)
     return check_user
 
+def create_new_user(username, password):
+    new_user = User(username, password)
+    return new_user
+
 def passlocker():
     print("------------------------------------------------------------------------------------------------------------------------ ")
     print("██████╗░░█████╗░░██████╗░██████╗░██╗░░░░░░░██╗░█████╗░██████╗░██████╗░  ██╗░░░░░░█████╗░░█████╗░██╗░░██╗███████╗██████╗░ ")
