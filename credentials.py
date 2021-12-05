cd7877a789c1f87c7df67b738599b2e886ea8af3
Steve Njuguna <stevenjuguna93@gmail.com>
2021-12-05 16:54:07 +0300

    Added delete credentials function

--- credentials.py
+++ credentials.py
@@ -8,3 +8,6 @@ class Credentials:
 
     def save_credentials(self):
         Credentials.credentials_list.append(self)
+
+    def delete_credentials(self):
+        Credentials.credentials_list.remove(self)
