4a36f10c4f41e56a58000271aaf6ad23f70a7947
Steve Njuguna <stevenjuguna93@gmail.com>
2021-12-06 11:18:52 +0300

    Added init test function

--- test_credential.py
+++ test_credential.py
@@ -6,5 +6,11 @@ class TestCredential(unittest.TestCase):
         '''setup method called before any test is run. We create a new credential asign value and test if its working fine'''
         self.new_credential = Credentials("IG", "STEVE", "12345")
 
+    def test_init(self):
+        ''''method runs whenever we instanciate our class so every time we instanciate our class we create a credential'''
+        self.assertEqual(self.new_credential.accountName, "IG")
+        self.assertEqual(self.new_credential.accountUsername, "STEVE")
+        self.assertEqual(self.new_credential.accountPassword, "12345")
+
 if __name__ == "__main__":
     unittest.main()
