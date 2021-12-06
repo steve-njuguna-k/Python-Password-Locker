a85a821fcc48a8de5c278fc00bf19390ca978aed
Steve Njuguna <stevenjuguna93@gmail.com>
2021-12-06 03:07:23 +0300

    Added setUp test function

--- test_user.py
+++ test_user.py
@@ -1,1 +1,11 @@
 import unittest
+from user import User
+
+class TestUser(unittest.TestCase):
+    def setUp(self):
+        '''setup method called before any test is run. We create a new user asign value and test if its working fine'''
+        self.user = User("STEVE", "123")
+
+
+if __name__ == '__main__':
+    unittest.main()
