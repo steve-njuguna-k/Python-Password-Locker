65b7fb2f2626ccc6954e2267f4019e894b0c8413
Steve Njuguna <stevenjuguna93@gmail.com>
2021-12-06 03:11:14 +0300

    Added test_init test function

--- test_user.py
+++ test_user.py
@@ -4,7 +4,11 @@ from user import User
 class TestUser(unittest.TestCase):
     def setUp(self):
         '''setup method called before any test is run. We create a new user asign value and test if its working fine'''
-        self.user = User("STEVE", "123")
+        self.new_user = User("Steve", "12345")
+
+    def test_init(self):
+        self.assertEqual(self.new_user.username, "Steve")
+        self.assertEqual(self.new_user.password, "12345")
 
 
 if __name__ == '__main__':
